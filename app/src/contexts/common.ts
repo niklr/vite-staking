491a1c7dcf8150f2cc79fb97dcdb0b04b36adf98
niklr <r.niklaus@gmx.ch>
2021-10-06 20:30:01 +0200

    Load datasource based on selected network

--- app/src/contexts/common.ts
+++ app/src/contexts/common.ts
@@ -1,23 +1,35 @@
 import { ViteClient } from "../clients/vite.client";
+import { IDataSource, ViteDataSource } from "../datasources";
+import { MockDataSource } from "../datasources/mock.datasource";
 import { getLogger } from "../util/logger";
 import { Network } from "../util/types";
 
-const logger = getLogger()
+const logger = getLogger();
 
 export class CommonContext {
-  private readonly _vite: ViteClient
+  private readonly _vite: ViteClient;
+  private _datasource: IDataSource;
 
   constructor() {
-    this._vite = new ViteClient()
+    this._vite = new ViteClient();
+    this._datasource = new MockDataSource();
   }
 
   async initAsync(network: Network): Promise<void> {
-    await this._vite.initAsync(network)
+    switch (network.id) {
+      case 3:
+        this._datasource = new MockDataSource();
+        break;
+      default:
+        this._datasource = new ViteDataSource();
+        break;
+    }
+    await this._vite.initAsync(network);
   }
 
   dispose(): void {
-    logger.info("Disposing CommonContext")()
-    this._vite.dispose()
+    logger.info("Disposing CommonContext")();
+    this._vite.dispose();
   }
 
   get vite(): ViteClient {
@@ -28,5 +40,5 @@ export class CommonContext {
 const context = new CommonContext();
 
 export const getCommonContext = () => {
-  return context
+  return context;
 }
--- app/src/datasources/mock.datasource.ts
+++ app/src/datasources/mock.datasource.ts
@@ -1,17 +1,13 @@
 import BigNumber from "bignumber.js";
-import { getViteClient, ViteClient } from "../clients/vite.client";
 import { getLogger } from "../util/logger";
 import { BaseDataSource } from "./base.datasource";
 
 const logger = getLogger();
 
-export class ViteDataSource extends BaseDataSource {
-  private readonly _client: ViteClient;
-
+export class MockDataSource extends BaseDataSource {
   constructor() {
     super();
-    this._client = getViteClient();
-    logger.info("ViteDataSource loaded")();
+    logger.info("MockDataSource loaded")();
   }
 
   getBalanceAsync(_address: string): Promise<BigNumber> {
