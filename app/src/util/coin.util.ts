a891dbf70b9595de61d7c25b344eb42f71683267
niklr <r.niklaus@gmx.ch>
2021-10-24 21:57:54 +0200

    Fix static coin icon urls

--- app/src/util/coin.util.ts
+++ app/src/util/coin.util.ts
@@ -5,9 +5,9 @@ export class CoinUtil {
 
   constructor() {
     this._iconUrlMap = new Map<string, string>([
-      ["https://static.vite.net/token-profile-1257137467/icon/e6dec7dfe46cb7f1c65342f511f0197c.png", "/assets/coins/vite.png"],
-      ["https://static.vite.net/token-profile-1257137467/icon/5faa1fa2b952137081e36c51af2b493c.png", "/assets/coins/usdc.png"],
-      ["https://static.vite.net/token-profile-1257137467/icon/3c231a8309999ad226afa097488a6158.png", "/assets/coins/dai.png"]
+      ["https://static.vite.net/token-profile-1257137467/icon/e6dec7dfe46cb7f1c65342f511f0197c.png", "./assets/coins/vite.png"],
+      ["https://static.vite.net/token-profile-1257137467/icon/5faa1fa2b952137081e36c51af2b493c.png", "./assets/coins/usdc.png"],
+      ["https://static.vite.net/token-profile-1257137467/icon/3c231a8309999ad226afa097488a6158.png", "./assets/coins/dai.png"]
     ]);
     this._coinMarketCapMap = new Map<string, string>();
     this._coingeckoMap = new Map<string, string>();
