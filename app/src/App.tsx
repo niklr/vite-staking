fc7ae000c4f06f6fb86227842953744f4017c7d7
niklr <r.niklaus@gmx.ch>
2021-10-30 09:34:37 +0200

    Update contract address and fix BigNumber.toString exponential notation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { SnackbarProvider } from 'notistack';
 import { ApolloProvider } from '@apollo/client';
 import { styled } from '@mui/material';
+import BigNumber from 'bignumber.js';
+import { SnackbarProvider } from 'notistack';
+import React from 'react';
+import BackgroundSVG from './bg.svg';
 import { getApolloClient } from './clients/apollo.client';
 import { ConnectedWeb3 } from './contexts/connectedWeb3';
 import { Web3Provider } from './contexts/web3';
 import { Main } from './features/main/components/main';
-import BackgroundSVG from './bg.svg';
 
 const Background = styled('div')(`
   width: 100%;
@@ -19,6 +20,8 @@ const Background = styled('div')(`
 
 const App: React.FC = () => {
   const apolloClient = React.useMemo(() => getApolloClient(), [])
+  // Almost never return exponential notation
+  BigNumber.config({ EXPONENTIAL_AT: 1e+9 })
   return (
     <Background>
       <SnackbarProvider maxSnack={3}>
